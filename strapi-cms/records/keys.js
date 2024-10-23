7dbc38d320781e072eba6433b1de4efde8fdb72a
Bill Somen <bill.somen@vircom.com>
2024-10-23 14:13:36 -0400

    app updates

---
+++ strapi-cms/records/keys.js
@@ -0,0 +1,626 @@
+{
+	"version": 2,
+	"data": {
+		"api::vp-tag.vp-tag": {},
+		"api::vp-produit.vp-produit": {},
+		"plugin::upload.file": {},
+		"plugin::upload.folder": {},
+		"api::vp-page.vp-page": {
+			"1": {
+				"id": 1,
+				"title": "Dashboard",
+				"uri": "index",
+				"description": "Hello world",
+				"name": "dashboard",
+				"createdAt": "2024-07-03T20:07:42.005Z",
+				"updatedAt": "2024-10-22T16:46:26.973Z",
+				"publishedAt": "2024-07-03T20:07:42.991Z",
+				"locale": "en",
+				"icon": "ri-dashboard-line",
+				"order": 0,
+				"vp_assets": [
+					1,
+					5
+				],
+				"localizations": [
+					3
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"2": {
+				"id": 2,
+				"title": "Alerts",
+				"uri": "alerts",
+				"description": "Hello world",
+				"name": "alerts",
+				"createdAt": "2024-07-04T17:24:04.187Z",
+				"updatedAt": "2024-10-22T16:46:38.833Z",
+				"publishedAt": "2024-07-04T17:24:05.383Z",
+				"locale": "en",
+				"icon": "ri-alert-line",
+				"order": 1,
+				"vp_assets": [
+					1
+				],
+				"localizations": [
+					4
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"3": {
+				"id": 3,
+				"title": "Tableau de bord",
+				"uri": "index",
+				"description": "Bonjour le monde",
+				"name": "dashboard",
+				"createdAt": "2024-10-03T19:34:41.532Z",
+				"updatedAt": "2024-10-22T16:46:26.958Z",
+				"publishedAt": "2024-08-05T16:33:03.526Z",
+				"locale": "fr",
+				"icon": "ri-dashboard-line",
+				"order": -1,
+				"vp_assets": [
+					2,
+					4
+				],
+				"localizations": [
+					1
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"4": {
+				"id": 4,
+				"title": "Alertes",
+				"uri": "alerts",
+				"description": "Salut le monde",
+				"name": "alerts",
+				"createdAt": "2024-10-03T19:34:41.582Z",
+				"updatedAt": "2024-10-22T16:46:38.849Z",
+				"publishedAt": "2024-08-07T21:31:35.795Z",
+				"locale": "fr",
+				"icon": "ri-alert-line",
+				"order": null,
+				"vp_assets": [
+					2
+				],
+				"localizations": [
+					2
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"5": {
+				"id": 5,
+				"title": "Profile",
+				"uri": "profile",
+				"description": null,
+				"name": "profile",
+				"createdAt": "2024-10-22T12:00:51.030Z",
+				"updatedAt": "2024-10-22T16:58:19.378Z",
+				"publishedAt": null,
+				"locale": "en",
+				"icon": "ri-user-3-line",
+				"order": 3,
+				"vp_assets": [],
+				"localizations": [
+					6
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"6": {
+				"id": 6,
+				"title": "Profile",
+				"uri": "profile",
+				"description": null,
+				"name": "profile",
+				"createdAt": "2024-10-22T12:02:51.366Z",
+				"updatedAt": "2024-10-22T16:58:19.405Z",
+				"publishedAt": null,
+				"locale": "fr",
+				"icon": "ri-user-3-line",
+				"order": null,
+				"vp_assets": [],
+				"localizations": [
+					5
+				],
+				"createdBy": null,
+				"updatedBy": null
+			}
+		},
+		"api::vp-asset.vp-asset": {
+			"1": {
+				"id": 1,
+				"key": "hello_world",
+				"value": "\nVircom \"Hello, World!\" program is generally a simple computer program that emits (or displays) to the screen (often the console) a message similar to \"Hello, World!\" while ignoring any user input. <script> test </script>\n\n**Bold** <u>Underline</u>",
+				"createdAt": "2024-07-03T20:05:58.381Z",
+				"updatedAt": "2024-10-03T19:34:42.008Z",
+				"publishedAt": "2024-07-03T20:06:02.849Z",
+				"locale": "en",
+				"vp_pages": [
+					1,
+					2
+				],
+				"localizations": [
+					2
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"2": {
+				"id": 2,
+				"key": "hello_world",
+				"value": "French programme « Hello, World ! » est généralement un programme informatique simple qui émet (ou affiche) à l'écran (souvent la console) un message similaire à « Hello, World ! » tout en ignorant toute entrée de l'utilisateur.",
+				"createdAt": "2024-10-03T19:34:41.718Z",
+				"updatedAt": "2024-10-03T19:34:42.107Z",
+				"publishedAt": "2024-08-05T16:32:08.873Z",
+				"locale": "fr",
+				"vp_pages": [
+					3,
+					4
+				],
+				"localizations": [
+					1
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"3": {
+				"id": 3,
+				"key": "button_all_customers",
+				"value": "All Customers",
+				"createdAt": "2024-09-13T01:14:56.422Z",
+				"updatedAt": "2024-10-03T19:34:42.030Z",
+				"publishedAt": "2024-09-13T01:21:44.981Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"4": {
+				"id": 4,
+				"key": "placeholder_search",
+				"value": "Rechercher...",
+				"createdAt": "2024-10-03T19:34:41.799Z",
+				"updatedAt": "2024-10-17T12:41:00.756Z",
+				"publishedAt": "2024-09-13T01:28:26.551Z",
+				"locale": "fr",
+				"vp_pages": [
+					3
+				],
+				"localizations": [
+					5
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"5": {
+				"id": 5,
+				"key": "placeholder_search",
+				"value": "Search...",
+				"createdAt": "2024-09-13T01:22:58.301Z",
+				"updatedAt": "2024-10-17T12:41:00.775Z",
+				"publishedAt": "2024-09-13T01:23:10.694Z",
+				"locale": "en",
+				"vp_pages": [
+					1
+				],
+				"localizations": [
+					4
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"6": {
+				"id": 6,
+				"key": "application_switcher",
+				"value": "Naviguez entre les applications",
+				"createdAt": "2024-10-03T19:34:41.858Z",
+				"updatedAt": "2024-10-17T12:48:43.547Z",
+				"publishedAt": "2024-10-17T12:48:43.543Z",
+				"locale": "fr",
+				"vp_pages": [],
+				"localizations": [
+					9
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"7": {
+				"id": 7,
+				"key": "application_switcher_description",
+				"value": "Get links to the other application in real time",
+				"createdAt": "2024-09-13T07:39:43.788Z",
+				"updatedAt": "2024-10-03T19:34:42.066Z",
+				"publishedAt": "2024-09-13T07:39:57.017Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"8": {
+				"id": 8,
+				"key": "button_logout",
+				"value": "Log out",
+				"createdAt": "2024-10-07T18:47:29.688Z",
+				"updatedAt": "2024-10-07T18:48:54.334Z",
+				"publishedAt": "2024-10-07T18:47:31.877Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [
+					10
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"9": {
+				"id": 9,
+				"key": "application_switcher",
+				"value": "Switch between applications",
+				"createdAt": "2024-09-13T07:41:01.163Z",
+				"updatedAt": "2024-10-17T12:48:43.559Z",
+				"publishedAt": "2024-09-13T07:41:13.007Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [
+					6
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"10": {
+				"id": 10,
+				"key": "button_logout",
+				"value": "Déconnection",
+				"createdAt": "2024-10-07T18:48:51.731Z",
+				"updatedAt": "2024-10-07T18:48:54.312Z",
+				"publishedAt": "2024-10-07T18:48:54.307Z",
+				"locale": "fr",
+				"vp_pages": [],
+				"localizations": [
+					8
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"11": {
+				"id": 11,
+				"key": "alertPage_mailFlowAlert",
+				"value": "Mail Flow Alerts",
+				"createdAt": "2024-10-17T12:42:21.224Z",
+				"updatedAt": "2024-10-17T12:43:14.329Z",
+				"publishedAt": "2024-10-17T12:42:33.147Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [
+					12
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"12": {
+				"id": 12,
+				"key": "alertPage_mailFlowAlert",
+				"value": "Alertes sur le flux de courrier",
+				"createdAt": "2024-10-17T12:43:11.741Z",
+				"updatedAt": "2024-10-17T12:43:14.312Z",
+				"publishedAt": "2024-10-17T12:43:14.306Z",
+				"locale": "fr",
+				"vp_pages": [],
+				"localizations": [
+					11
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"13": {
+				"id": 13,
+				"key": "alertPage_description",
+				"value": "Alerts about mail flow such as inbound or outbound abnormal traffic, spam or virus being sent as well as reputation or your server. ",
+				"createdAt": "2024-10-17T12:44:42.480Z",
+				"updatedAt": "2024-10-17T12:45:45.104Z",
+				"publishedAt": "2024-10-17T12:44:44.168Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [
+					14
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"14": {
+				"id": 14,
+				"key": "alertPage_description",
+				"value": "Alertes sur les flux de courrier tels que le trafic anormal entrant ou sortant, l'envoi de spam ou de virus, ainsi que sur la réputation de votre serveur. ",
+				"createdAt": "2024-10-17T12:45:41.611Z",
+				"updatedAt": "2024-10-17T12:45:45.090Z",
+				"publishedAt": "2024-10-17T12:45:45.085Z",
+				"locale": "fr",
+				"vp_pages": [],
+				"localizations": [
+					13
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"15": {
+				"id": 15,
+				"key": "alertTableTilte_title",
+				"value": "Title",
+				"createdAt": "2024-10-17T12:46:54.741Z",
+				"updatedAt": "2024-10-17T12:48:27.540Z",
+				"publishedAt": "2024-10-17T12:47:09.210Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [
+					16
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"16": {
+				"id": 16,
+				"key": "alertTableTilte_title",
+				"value": "Titre",
+				"createdAt": "2024-10-17T12:47:18.825Z",
+				"updatedAt": "2024-10-17T12:48:27.528Z",
+				"publishedAt": "2024-10-17T12:48:27.524Z",
+				"locale": "fr",
+				"vp_pages": [],
+				"localizations": [
+					15
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"17": {
+				"id": 17,
+				"key": "alertTableTitle_customer",
+				"value": "Customer",
+				"createdAt": "2024-10-17T12:47:47.397Z",
+				"updatedAt": "2024-10-17T12:48:13.904Z",
+				"publishedAt": "2024-10-17T12:47:50.170Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [
+					18
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"18": {
+				"id": 18,
+				"key": "alertTableTitle_customer",
+				"value": "Client",
+				"createdAt": "2024-10-17T12:48:00.025Z",
+				"updatedAt": "2024-10-17T12:48:13.892Z",
+				"publishedAt": "2024-10-17T12:48:13.888Z",
+				"locale": "fr",
+				"vp_pages": [],
+				"localizations": [
+					17
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"19": {
+				"id": 19,
+				"key": "alertTableTitle_severity",
+				"value": null,
+				"createdAt": "2024-10-17T13:37:41.390Z",
+				"updatedAt": "2024-10-17T13:45:15.955Z",
+				"publishedAt": "2024-10-17T13:37:43.786Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [
+					20
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"20": {
+				"id": 20,
+				"key": "alertTableTitle_severity",
+				"value": "Sévérité",
+				"createdAt": "2024-10-17T13:45:10.876Z",
+				"updatedAt": "2024-10-17T13:45:15.941Z",
+				"publishedAt": "2024-10-17T13:45:15.936Z",
+				"locale": "fr",
+				"vp_pages": [],
+				"localizations": [
+					19
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"21": {
+				"id": 21,
+				"key": "alertTableTitle_timestamp",
+				"value": "Timestamp",
+				"createdAt": "2024-10-17T13:46:26.882Z",
+				"updatedAt": "2024-10-17T13:46:50.345Z",
+				"publishedAt": "2024-10-17T13:46:28.533Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [
+					22
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"22": {
+				"id": 22,
+				"key": "alertTableTitle_timestamp",
+				"value": "Horodatage",
+				"createdAt": "2024-10-17T13:46:42.184Z",
+				"updatedAt": "2024-10-17T13:46:50.330Z",
+				"publishedAt": "2024-10-17T13:46:43.851Z",
+				"locale": "fr",
+				"vp_pages": [],
+				"localizations": [
+					21
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"23": {
+				"id": 23,
+				"key": "alertTableTitle_status",
+				"value": "Status",
+				"createdAt": "2024-10-17T13:47:38.685Z",
+				"updatedAt": "2024-10-17T13:48:10.897Z",
+				"publishedAt": "2024-10-17T13:47:40.380Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [
+					24
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"24": {
+				"id": 24,
+				"key": "alertTableTitle_status",
+				"value": "Statut",
+				"createdAt": "2024-10-17T13:47:54.004Z",
+				"updatedAt": "2024-10-17T13:48:10.882Z",
+				"publishedAt": "2024-10-17T13:48:10.878Z",
+				"locale": "fr",
+				"vp_pages": [],
+				"localizations": [
+					23
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"25": {
+				"id": 25,
+				"key": "alertTableTitle_parent",
+				"value": "Parent",
+				"createdAt": "2024-10-21T03:00:14.315Z",
+				"updatedAt": "2024-10-21T03:00:30.360Z",
+				"publishedAt": "2024-10-21T03:00:15.215Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [
+					26
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"26": {
+				"id": 26,
+				"key": "alertTableTitle_parent",
+				"value": "Parent",
+				"createdAt": "2024-10-21T03:00:23.562Z",
+				"updatedAt": "2024-10-21T03:00:30.344Z",
+				"publishedAt": "2024-10-21T03:00:25.091Z",
+				"locale": "fr",
+				"vp_pages": [],
+				"localizations": [
+					25
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"27": {
+				"id": 27,
+				"key": "alertTableTitle_severity",
+				"value": "Severity",
+				"createdAt": "2024-10-21T03:01:22.708Z",
+				"updatedAt": "2024-10-21T03:01:50.967Z",
+				"publishedAt": "2024-10-21T03:01:23.713Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [
+					28
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"28": {
+				"id": 28,
+				"key": "alertTableTitle_severity",
+				"value": "Sévérité",
+				"createdAt": "2024-10-21T03:01:49.912Z",
+				"updatedAt": "2024-10-21T03:01:50.949Z",
+				"publishedAt": "2024-10-21T03:01:50.945Z",
+				"locale": "fr",
+				"vp_pages": [],
+				"localizations": [
+					27
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"29": {
+				"id": 29,
+				"key": "alertTableTitle_status",
+				"value": "Status",
+				"createdAt": "2024-10-21T03:02:38.421Z",
+				"updatedAt": "2024-10-21T03:02:51.472Z",
+				"publishedAt": "2024-10-21T03:02:39.635Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [
+					30
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"30": {
+				"id": 30,
+				"key": "alertTableTitle_status",
+				"value": "Statut",
+				"createdAt": "2024-10-21T03:02:50.337Z",
+				"updatedAt": "2024-10-21T03:02:51.461Z",
+				"publishedAt": "2024-10-21T03:02:51.457Z",
+				"locale": "fr",
+				"vp_pages": [],
+				"localizations": [
+					29
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"31": {
+				"id": 31,
+				"key": "navLink_suport",
+				"value": "Support",
+				"createdAt": "2024-10-22T21:10:35.723Z",
+				"updatedAt": "2024-10-22T21:10:50.575Z",
+				"publishedAt": "2024-10-22T21:10:40.316Z",
+				"locale": "en",
+				"vp_pages": [],
+				"localizations": [
+					32
+				],
+				"createdBy": null,
+				"updatedBy": null
+			},
+			"32": {
+				"id": 32,
+				"key": "navLink_suport",
+				"value": "Support",
+				"createdAt": "2024-10-22T21:10:49.050Z",
+				"updatedAt": "2024-10-22T21:10:50.561Z",
+				"publishedAt": "2024-10-22T21:10:50.554Z",
+				"locale": "fr",
+				"vp_pages": [],
+				"localizations": [
+					31
+				],
+				"createdBy": null,
+				"updatedBy": null
+			}
+		},
+		"api::vp-new.vp-new": {},
+		"api::vp-app.vp-app": {},
+		"api::vp-alert.vp-alert": {}
+	}
+}
